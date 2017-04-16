4fac135715c9aec9f512f19ec0b0c8ab2a60b7b0
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-04-16 14:02:31 -0600

    ES6-ify

---
+++ app/utils/api.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function getProfile (username) {
+  return axios.get(`https://api.github.com/users/${username}${params}`)
+    .then(({ data }) => data)
+}
+
+function getRepos (username) {
+  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce((count, { stargazers_count }) => count + stargazers_count, 0);
+}
+
+function calculateScore ({ followers }, repos) {
+  const totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError (error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData (player) {
+  return Promise.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([ profile, repos ]) => {
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+function sortPlayers (players) {
+  return players.sort((a,b) => b.score - a.score);
+}
+
+export function battle (players) {
+  return Promise.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError);
+}
+
+export function fetchPopularRepos (language) {
+  const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+  return axios.get(encodedURI)
+    .then(({ data }) => data.items);
+}
