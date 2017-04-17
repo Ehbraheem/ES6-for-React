cfe77084929b16116a878107e3598779ffea279d
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-04-16 18:30:19 -0600

    async-await

--- app/components/Results.js
+++ app/components/Results.js
@@ -49,25 +49,25 @@ class Results extends React.Component {
     error: null,
     loading: true,
   }
-  componentDidMount() {
+  async componentDidMount() {
     const { playerOneName, playerTwoName } = parse(this.props.location.search);
 
-    battle([
-     playerOneName,
-     playerTwoName
-    ]).then((players) => (
-      players === null
-        ? this.setState(() => ({
-            error: 'Looks like there was an error. Check that both users exist on Github.',
-            loading: false,
-          }))
-        : this.setState(() => ({
-            error: null,
-            winner: players[0],
-            loser: players[1],
-            loading: false,
-          }))
-    ))
+    const players = await battle([
+      playerOneName,
+      playerTwoName
+    ]);
+
+    return players === null
+      ? this.setState(() => ({
+          error: 'Looks like there was an error. Check that both users exist on Github.',
+          loading: false,
+        }))
+      : this.setState(() => ({
+          error: null,
+          winner: players[0],
+          loser: players[1],
+          loading: false,
+        }))
   }
   render() {
     const { error, winner, loser, loading } = this.state;
